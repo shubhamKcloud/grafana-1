0b4bfe34be4c80b837c35447b87a41eacd406070
shubhamKcloud <kumarshubham1807@gmail.com>
2023-05-14 23:13:28 +0530

    update

--- .github/tvarit/deploy_to_dev_sk.sh
+++ .github/tvarit/deploy_to_dev_sk.sh
@@ -119,8 +119,8 @@ docker push 047870419389.dkr.ecr.eu-central-1.amazonaws.com/lightsail:latest
 echo "create lightsail instance..."
 #AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value --secret-id /credentials/grafana-user/access-key --output text --query SecretString)
 #AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value --secret-id /credentials/grafana-user/secret-key --output text --query SecretString)
-sed -i "s#<AWS_ACCESS_KEY/>#${AWS_ACCESS_KEY}#g" instance.sh
-sed -i "s#<AWS_SECRET_KEY/>#${AWS_SECRET_KEY}#g" instance.sh
+sed -i "s#<AWS_ACCESS_KEY/>#${AWS_ACCESS_KEY}#g" lightsail.sh
+sed -i "s#<AWS_SECRET_KEY/>#${AWS_SECRET_KEY}#g" lightsail.sh
 cat instance.sh
 #TODO: Check If instance already exit
 aws lightsail create-instances --instance-names grafana-${PREFIX} --availability-zone eu-central-1a --blueprint-id ubuntu_22_04 --bundle-id nano_2_0 --user-data file://lightsail.sh
