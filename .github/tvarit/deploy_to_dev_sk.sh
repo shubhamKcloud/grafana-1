9bf0e3e909dc17214e06c603b6d4dde024449d5c
shubhamKcloud <kumarshubham1807@gmail.com>
2023-05-17 00:16:10 +0530

    update

--- .github/tvarit/deploy_to_dev_sk.sh
+++ .github/tvarit/deploy_to_dev_sk.sh
@@ -150,8 +150,6 @@ if [ $return_value_instance -eq 0 ]; then
 else
     echo "lightsail instance does not exist. Creating instance!!!!!!"
     cp lightsail.sh userdata.sh
-    AWS_ACCESS_KEY_ID=$(aws secretsmanager get-secret-value --secret-id /credentials/grafana-user/access-key --output text --query SecretString)
-    AWS_SECRET_ACCESS_KEY=$(aws secretsmanager get-secret-value --secret-id /credentials/grafana-user/secret-key --output text --query SecretString)
     sed -i "s#<AWS_ACCESS_KEY/>#${AWS_ACCESS_KEY}#g" userdata.sh
     sed -i "s#<AWS_SECRET_KEY/>#${AWS_SECRET_KEY}#g" userdata.sh
     cat userdata.sh
